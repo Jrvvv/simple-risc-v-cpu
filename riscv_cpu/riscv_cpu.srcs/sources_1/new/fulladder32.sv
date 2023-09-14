c9d448e8f7a66ff19e23f431cc27f967a528a331
Jrvvv <JorikJorik2000@gmail.com>
2023-09-14 04:26:36 +0300

    minor

---
+++ riscv_cpu/riscv_cpu.srcs/sources_1/new/fulladder32.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/14/2023 02:09:11 AM
+// Design Name: 
+// Module Name: fulladder32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fulladder32(
+
+    );
+endmodule
