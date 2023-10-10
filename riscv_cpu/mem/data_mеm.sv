c91d06dec4545e541638839a4cc8b2ae36a382d6
jrvvv <JorikJorik2000@gmail.com>
2023-10-10 17:02:31 +0300

    instr mem done and init file uploaded

---
+++ riscv_cpu/mem/data_mеm.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/10/2023 04:33:17 PM
+// Design Name: 
+// Module Name: data_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_mem(
+
+    );
+endmodule
---
+++ riscv_cpu/mem/instr_mеm.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/10/2023 04:33:17 PM
+// Design Name: 
+// Module Name: instr_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instr_mem
+(
+    input   logic [31:0] addr_i,
+    output  logic [31:0] read_data_o 
+);
+
+    logic [31:0] RAM [1023:0];
+    
+    initial
+        $readmemh("program.txt", RAM);
+        
+    assign read_data_o = (addr_i < 32'd4095) ? RAM[addr_i[31:2]] : 32'd0;
+    
+endmodule
