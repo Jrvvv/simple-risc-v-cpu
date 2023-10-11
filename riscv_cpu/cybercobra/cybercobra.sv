bd398100d75eb9d066a44c18f75876a02af8d10b
jrvvv <JorikJorik2000@gmail.com>
2023-10-12 00:54:11 +0300

    raw cobra module, tb and board + example prog

---
+++ riscv_cpu/cybercobra/cybercobra.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2023 12:48:57 AM
+// Design Name: 
+// Module Name: cybercobra
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CYBERcobra
+(
+    input  logic            clk_i,
+    input  logic            rst_i,
+    input  logic    [15:0]  sw_i,
+    output logic    [31:0]  out_o
+);
+    
+    
+
+endmodule
