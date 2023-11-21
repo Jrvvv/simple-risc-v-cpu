9ef5f05b8eb6db1390b93137489983dee2209100
jrvvv <JorikJorik2000@gmail.com>
2023-11-21 23:42:40 +0300

    in cpu unit ext_mem instaed of data_mem, LSU prototype done + disable files from older labs

--- riscv_cpu/LSU/riscv_lsu.sv
+++ riscv_cpu/LSU/riscv_lsu.sv
@@ -22,6 +22,80 @@ module riscv_lsu
   input  logic        mem_ready_i
 );
 
+    import riscv_pkg::*;
 
+    logic stall_reg;
+    
+    // wires
+    logic [1:0] byte_offset;
+    
+    // assignin wires
+    assign byte_offset = core_addr_i[1:0];
+    
+    // stright from in to out
+    assign mem_req_o    = core_req_i;
+    assign mem_we_o     = core_we_i; 
+    assign core_addr_o  = mem_addr_o;
+    
+    // setting stall out value
+    assign core_stall_o = core_req_i & ~(mem_ready_i & stall_reg);
+    // setting stall reg value
+    always_ff@(posedge clk_i)
+        stall_reg <= core_stall_o;
+  
+  // LDST_B          = 3'b000; Знаковое 8-битное значение
+  // LDST_H          = 3'b001; Знаковое 16-битное значение
+  // LDST_W          = 3'b010; 32-битное значение
+  // LDST_BU         = 3'b100; Беззнаковое 8-битное значение
+  // LDST_HU         = 3'b101; Беззнаковое 16-битное значение
+    
+    // setting mem_be_o value
+    always_comb begin
+        case (core_size_i)
+            LDST_B: begin
+            end
+            
+            LDST_H: begin
+            end
+            
+            LDST_W: begin
+            end
+            
+            default: mem_be_o <= mem_be_o;
+        endcase
+        
+    end
+    
+    // setting core_rd_o value
+    always_comb begin
+        case (core_size_i)
+            LDST_B: begin
+            end
+            
+            LDST_H: begin
+            end
+            
+            LDST_W: begin
+            end
+            
+            default: mem_be_o <= mem_be_o;
+        endcase
+    end
+    
+    // setting mem_wd_o value
+    always_comb begin
+        case (core_size_i)
+            LDST_B: begin
+            end
+            
+            LDST_H: begin
+            end
+            
+            LDST_W: begin
+            end
+            
+            default: mem_be_o <= mem_be_o;
+        endcase
+    end
 
 endmodule
--- riscv_cpu/datapath/riscv_unit.sv
+++ riscv_cpu/datapath/riscv_unit.sv
@@ -6,17 +6,30 @@ module riscv_unit
     input  logic        rst_i
 );
 
-    logic           stall_reg;
-    logic           stall;
+    // core <-> LSU wires
+    // from core to LSU
+    logic           core_req;
+    logic           core_we;
+    logic   [2: 0]  core_size;
+    logic   [31:0]  core_wd;
+    logic   [31:0]  core_addr;
+    // to core from LSU
+    logic           core_stall;
+    logic   [31:0]  core_rd;
 
+    // LSU <-> data mem wires
+    // from LSU to data mem
+    logic           data_req;
+    logic           data_we;
+    logic   [3: 0]  data_be;
+    logic   [31:0]  data_wd;
+    logic   [31:0]  data_addr;
+    // to LSU from data mem
+    logic           data_ready;
+    logic   [31:0]  data_rd;
+
+    // instr mem <-> core wires
     logic   [31:0]  instr_addr;
-    logic           mem_req;
-    logic           mem_we;
-    // logic   [2: 0]  mem_size;
-    logic   [31:0]  mem_wd;
-    logic   [31:0]  mem_addr;
-
-    logic   [31:0]  mem_rd;
     logic   [31:0]  instr;
 
     riscv_core core
@@ -24,37 +37,56 @@ module riscv_unit
         .clk_i          (clk_i),
         .rst_i          (rst_i),
 
-        .stall_i        (stall),
+        .stall_i        (core_stall),
         .instr_i        (instr),
-        .mem_rd_i       (mem_rd),
+        .mem_rd_i       (core_rd),
 
         .instr_addr_o   (instr_addr),
-        .mem_addr_o     (mem_addr),
-        // .mem_size_o     (),
-        .mem_req_o      (mem_req),
-        .mem_we_o       (mem_we),
-        .mem_wd_o       (mem_wd)
+        .mem_addr_o     (core_addr),
+        .mem_size_o     (core_size),
+        .mem_req_o      (core_req),
+        .mem_we_o       (core_we),
+        .mem_wd_o       (core_wd)
     );
 
-     instr_mem instr_mem_dev
-     (
-        .addr_i      (instr_addr),
-        .read_data_o (instr)
-     );
-
-    data_mem data_mem_dev
+    riscv_lsu lsu
     (
         .clk_i          (clk_i),
-        .mem_req_i      (mem_req),
-        .write_enable_i (mem_we),
-        .addr_i         (mem_addr),
-        .write_data_i   (mem_wd),
-        .read_data_o    (mem_rd)
+        .rst_i          (rst_i),
+
+        .core_req_i     (core_req),
+        .core_we_i      (core_we),
+        .core_size_i    (core_size),
+        .core_addr_i    (core_addr),
+        .core_wd_i      (core_wd),
+        .core_rd_o      (core_rd),
+        .core_stall_o   (core_stall),
+
+        .mem_req_o      (data_req),
+        .mem_we_o       (data_we),
+        .mem_be_o       (data_be),
+        .mem_addr_o     (data_addr),
+        .mem_wd_o       (data_wd),
+        .mem_rd_i       (data_rd),
+        .mem_ready_i
     );
 
-    assign stall = ~(stall_reg) & mem_req;
+    instr_mem instr_mem_dev
+    (
+        .addr_i      (instr_addr),
+        .read_data_o (instr)
+    );
 
-    always_ff@(posedge clk_i)
-        stall_reg <= stall;
+    ext_mem ext_mem_dev
+    (
+        .clk_i          (clk_i),
+        .mem_req_i      (data_req),
+        .write_enable_i (data_we),
+        .byte_enable_i  (data_be),
+        .addr_i         (data_addr),
+        .write_data_i   (data_wd),
+        .read_data_o    (data_rd),
+        .ready_o        (data_ready)
+    );
 
 endmodule
