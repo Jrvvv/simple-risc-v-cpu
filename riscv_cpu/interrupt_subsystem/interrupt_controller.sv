7ef413e63da55cb1ebb5edf08c517eca19525b01
Jrvvv <JorikJorik2000@gmail.com>
2023-12-07 15:22:45 +0300

    irq cntrlr partly done

--- riscv_cpu/interrupt_subsystem/interrupt_controller.sv
+++ riscv_cpu/interrupt_subsystem/interrupt_controller.sv
@@ -14,6 +14,22 @@ module interrupt_controller
     output logic [31:0] irq_cause_o,
     output logic        irq_o
 );
-    
-    
+
+    logic irq_h;
+    logic exc_h;
+
+    assign irq_cause_o  = 32'h1000_0010;
+    assign irq_o        = (irq_req_i & mie_i) & ~(irq_h | (exception_i | exc_h));
+    assign irq_ret_o    = mret_i & ~(exception_i | exc_h);
+
+    always_ff @(posedge clk_i) begin
+        if (rst_i) begin
+            exc_h <= 1'b0;
+            irq_h <= 1'b0;
+        end else begin
+            exc_h <= ~mret_i & (exception_i | exc_h);
+            irq_h <= 
+        end
+    end
+
 endmodule
