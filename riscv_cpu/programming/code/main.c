7ccc3835904fe429792e21602f04a9f76ca2aa24
jrvvv <JorikJorik2000@gmail.com>
2024-01-20 01:17:07 +0300

    loops to while changed

--- riscv_cpu/programming/code/main.c
+++ riscv_cpu/programming/code/main.c
@@ -1,20 +1,32 @@
 #include "platform.h"
 
-void init_color(volatile uint8_t* color_arr, uint8_t intns, uint8_t text, uint8_t back) {
-  for (int i = 0; i < MAP_HEIGHT * MAP_WIDTH; i++) {
-    color_arr[i] = GET_COLOR(intns, text, back);
+void init_color(uint8_t intns, uint8_t text, uint8_t back) {
+  int i;
+  i = 0;
+
+  while (i < MAP_HEIGHT * MAP_WIDTH) {
+    color_map[i] = GET_COLOR(intns, text, back);
+    i++;
   }
 }
 
 void init_text_ch(char ch) {
-  for (int i = 0; i < MAP_HEIGHT * MAP_WIDTH; i++) {
+  int i;
+  i = 0;
+
+  while (i < MAP_HEIGHT * MAP_WIDTH) {
     char_map[i] = ch;
+    i++;
   }
 }
 
-void init_text(const uint8_t* str_arr) {
-  for (int i = 0; i < MAP_HEIGHT * MAP_WIDTH; i++) {
+void init_text(uint8_t str_arr[]) {
+  int i;
+  i = 0;
+
+  while (i < MAP_HEIGHT * MAP_WIDTH) {
     char_map[i] = str_arr[i];
+    i++;
   }
 }
 
@@ -24,9 +36,9 @@ int main(int argc, char** argv) {
 
   finish = 0;
 
-  ps2_ptr->rst = 1;
+  // ps2_ptr->rst = 1;
 
-  init_color(color_map, FADE_MODE, CIN_CLR, WHT_CLR);
+  init_color(FADE_MODE, CIN_CLR, WHT_CLR);
   // TODO: check this case if doesn't work
   // or even
   // init_color(color_arr, FADE_MODE, CIN_CLR, WHT_CLR);
@@ -40,7 +52,7 @@ int main(int argc, char** argv) {
   while(1) {                               // В бесконечном цикле
     // init_text_ch('a');
     if (ps2_ptr->unread_data && !finish) {
-      init_color(color_map, FADE_MODE, RED_CLR, WHT_CLR);
+      init_color(FADE_MODE, RED_CLR, WHT_CLR);
       init_text(g_goodbye_str);
       // init_text_ch('b');
       finish = 1;
@@ -49,8 +61,6 @@ int main(int argc, char** argv) {
 
   return 0;
 }
-
-#define DEADLY_SERIOUS_EVENT 0xDEADDAD1
 
 void int_handler() {
 
